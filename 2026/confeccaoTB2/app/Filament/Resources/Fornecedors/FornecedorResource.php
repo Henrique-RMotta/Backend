9c4d9e7f17df4e28fb4f5704744c6f318e0680db
Henrique-RMotta <henrique2008790@gmail.com>
2026-03-23 16:35:06 -0300

    confeccao 2 pt2

---
+++ 2026/confeccaoTB2/app/Filament/Resources/Fornecedors/FornecedorResource.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Filament\Resources\Fornecedors;
+
+use App\Filament\Resources\Fornecedors\Pages\CreateFornecedor;
+use App\Filament\Resources\Fornecedors\Pages\EditFornecedor;
+use App\Filament\Resources\Fornecedors\Pages\ListFornecedors;
+use App\Filament\Resources\Fornecedors\Schemas\FornecedorForm;
+use App\Filament\Resources\Fornecedors\Tables\FornecedorsTable;
+use App\Models\Fornecedor;
+use BackedEnum;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\CheckboxList;
+
+class FornecedorResource extends Resource
+{
+    protected static ?string $model = Fornecedor::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+
+    protected static ?string $recordTitleAttribute = 'Fornecedor';
+
+    public static function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                textInput::make("FR_nome")->required()->label('Nome Completo'),
+                textInput::make("FR_email")->required()->email()->label('Email'),
+                textInput::make('FR_telefone')->tel()->label('Telefone'),
+                textInput::make("FR_CNPJ")->label('CPF ou CNPJ'),
+                CheckboxList::make('FR_tipo')->label('Tipo de Fornecedor')
+                    ->options([
+                        'alimentacao' => 'Alimentação',
+                        'producao' => 'Produção',
+                        'manutencao' => 'Manutenção',
+                        'predial' => 'Predial',
+                    ]),
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                textColumn::make('FR_nome')->label('Nome do Fornecedor')->searchable()->sortable(),
+                textColumn::make('FR_email')->label('Email do Fornecedor')->searchable()->sortable(),
+                textColumn::make('FR_telefone')->label('Telefone do Fornecedor')->searchable()->sortable(),
+                textColumn::make('FR_CNPJ')->label('CNPJ do Fornecedor')->searchable()->sortable(),
+                textColumn::make('FR_tipo')->label('Tipo de fornecedor')->searchable()->sortable(),
+
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListFornecedors::route('/'),
+            'create' => CreateFornecedor::route('/create'),
+            'edit' => EditFornecedor::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getRecordRouteBindingEloquentQuery(): Builder
+    {
+        return parent::getRecordRouteBindingEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
